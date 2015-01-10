16d1c3c484b9a15be57bcec990d56239e7f70295
allfake <allfake24@gmail.com>
2015-01-11 00:30:45 +0700

    add write data

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ var noble = require('noble');
 var isConnectSocket = false;
 var reScan = false;
 var serial_number = 'CCC'; 
+
+var peripheral_action = {};
+
 //var domain = "192.168.21.127";
 var domain = "http://192.168.21.127:9000";
 //var domain = "https://derconnect.herokuapp.com";
@@ -43,7 +46,106 @@ noble.on('discover', function(peripheral) {
     data: { name: peripheral.advertisement.localName, uuid: peripheral.uuid } 
   }
 
-  socket.emit("pi:receive", sendData);
+  if (peripheral_action && peripheral_action.peripheral_uuid == peripheral.uuid) {
+    console.log("match uuid");
+    noble.stopScanning();
+    peripheral.connect();
+  } else {
+    socket.emit("pi:receive", sendData);  
+  }
+
+  
+  peripheral.on('connect', function() {
+    console.log('on -> connect');
+    this.updateRssi();
+  });
+
+  peripheral.on('disconnect', function() {
+    console.log('on -> disconnect');
+  });
+
+  peripheral.on('rssiUpdate', function(rssi) {
+    console.log('on -> RSSI update ' + rssi);
+    this.discoverServices(['ffe0']);
+  });
+
+  peripheral.on('servicesDiscover', function(services) {
+    console.log('on -> peripheral services discovered ' + services);
+
+
+    var serviceIndex = 0;
+
+    services[serviceIndex].on('includedServicesDiscover', function(includedServiceUuids) {
+      console.log('on -> service included services discovered ' + includedServiceUuids);
+      this.discoverCharacteristics(['ffe1']);
+    });
+
+    services[serviceIndex].on('characteristicsDiscover', function(characteristics) {
+      console.log('on -> service characteristics discovered ' + characteristics);
+
+        characteristics[0].write(new Buffer([0x32]), false, function(error) {
+          console.log('write 0x32');
+        });
+      var characteristicIndex = 0;
+
+      characteristics[characteristicIndex].on('read', function(data, isNotification) {
+        console.log('on -> characteristic read ' + data + ' ' + isNotification);
+        console.log(data);
+
+        // peripheral.disconnect();
+      });
+
+      characteristics[characteristicIndex].on('write', function() {
+        console.log('on -> characteristic write ');
+
+        // peripheral.disconnect();
+      });
+
+      characteristics[characteristicIndex].on('broadcast', function(state) {
+        console.log('on -> characteristic broadcast ' + state);
+
+        // peripheral.disconnect();
+      });
+
+      characteristics[characteristicIndex].on('notify', function(state) {
+        console.log('on -> characteristic notify ' + state);
+
+        // peripheral.disconnect();
+      });
+
+      characteristics[characteristicIndex].on('descriptorsDiscover', function(descriptors) {
+        console.log('on -> descriptors discover ' + descriptors);
+
+        var descriptorIndex = 0;
+        console.log(descriptors);
+
+        //descriptors[descriptorIndex].on('valueRead', function(data) {
+        //  console.log('on -> descriptor value read ' + data);
+        //  console.log(data);
+        //  // peripheral.disconnect();
+        //});
+
+//        descriptors[descriptorIndex].on('valueWrite', function() {
+ //         console.log('on -> descriptor value write ');
+          // peripheral.disconnect();
+  //      });
+
+        //descriptors[descriptorIndex].readValue();
+        //descriptors[descriptorIndex].writeValue(new Buffer([0]));
+      });
+
+
+      // characteristics[characteristicIndex].read();
+      characteristics[characteristicIndex].write(new Buffer(peripheral_action.data));
+      //characteristics[characteristicIndex].broadcast(true);
+      // characteristics[characteristicIndex].notify(true);
+      // characteristics[characteristicIndex].discoverDescriptors();
+    });
+
+
+    services[serviceIndex].discoverIncludedServices();
+  });
+
 
 });
 
@@ -56,7 +158,14 @@ noble.on('scanStop', function() {
 
 });
 
-socket.on('pi:action:bleReScan:' + serial_number, function() {
+socket.on('pi:action:ble:' + serial_number, function(data) {
+  console.log('write peripheral:' + peripheral.serial_number + " with data:" + data.data);
+  peripheral_action = data;
+  reScan = true;
+  noble.stopScanning();
+});
+
+socket.on('pi:action:bleReScan:' + serial_number, function(data) {
   console.log('re scan');
   reScan = true;
   noble.stopScanning();
@@ -74,6 +183,22 @@ socket.on('disconnect', function(){
   noble.stopScanning();
 });
 
+
+  ////////////////////////////
+ /////// write data /////////
+////////////////////////////
+
+var writeFile = function (data) {
+
+  var filePath = path.join(__dirname, '/data/') + 'SENSOR-ble' ;
+  fs.writeFile(filePath, data), function(err) {
+      if(err) {
+          console.log(err);
+      } else {
+          console.log("The file was saved!");
+      }
+  }); 
+}
 
   ////////////////////////////
  /////// read data //////////
