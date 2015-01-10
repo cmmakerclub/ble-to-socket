3741ab6463f5694e9415761903c10ab499e655cd
allfake <allfake24@gmail.com>
2015-01-10 19:28:12 +0700

    fix missing socket connect

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ var serial_number = 'BBB';
 
 var isConnectSocket = false;
 
+var serial_number = 'BBB'; 
+// var domain = "http://localhost:9000/";
+var domain = "https://derconnect.herokuapp.com";
+
+socketClient = socketClient.connect(domain, { path: '/socket.io-client', query: "from=raspberry&serial_number=" + serial_number });
+
 noble.on('stateChange', function(state) {
 
   if (!isConnectSocket) {
